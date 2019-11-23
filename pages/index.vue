54e0f5fc198de1ec69bed62c820ef4ede3256d06
Leandro Nascimento <fleandro.nascimento@gmail.com>
2019-11-23 01:54:49 -0300

    adding basic structure login

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,30 @@
 <template>
   <div>
-    <h1>KDTI</h1>
+    <div v-if="$store.state.auth">
+      <p>
+        You are authenticated. You can see the
+        <NuxtLink to="/secret"> secret page </NuxtLink>!
+      </p>
+      <button @click="logout">
+        Logout
+      </button>
+    </div>
+    <p v-else>
+      Please
+      <NuxtLink to="/login"> login </NuxtLink>.
+    </p>
   </div>
 </template>
+
+<script>
+const Cookie = process.client ? require('js-cookie') : undefined
+
+export default {
+  methods: {
+    logout() {
+      Cookie.remove('auth')
+      this.$store.commit('setAuth', null)
+    },
+  },
+}
+</script>
---
+++ store/index.js
@@ -0,0 +1,26 @@
+const cookieparser = process.server ? require('cookieparser') : undefined
+
+export const state = () => {
+  return {
+    auth: null,
+  }
+}
+export const mutations = {
+  setAuth(state, auth) {
+    state.auth = auth
+  },
+}
+export const actions = {
+  nuxtServerInit({ commit }, { req }) {
+    let auth = null
+    if (req.headers.cookie) {
+      const parsed = cookieparser.parse(req.headers.cookie)
+      try {
+        auth = JSON.parse(parsed.auth)
+      } catch (err) {
+        // No valid cookie found
+      }
+    }
+    commit('setAuth', auth)
+  },
+}
