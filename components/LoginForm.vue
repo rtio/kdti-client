4e006811f5aef4976903127f018e8224970831ca
Carlos José <carlosxjm@gmail.com>
2019-11-24 19:19:16 -0300

    Login form validation

--- components/LoginForm.vue
+++ components/LoginForm.vue
@@ -1,25 +1,63 @@
 <template>
-  <form v-if="!isNewUser" class="form-small" action="POST">
+  <form
+    v-if="!isNewUser"
+    @submit.prevent="checkForm"
+    class="form-small"
+    novalidate
+  >
     <h3>Login</h3>
     <label for="email">E-mail</label>
-    <input name="email" type="email" />
+    <input v-model="email" name="email" type="email" />
+    <p class="error">{{ errors.email | first }}</p>
     <label for="password">Senha</label>
-    <input name="password" type="password" />
+    <input v-model="password" name="password" type="password" />
+    <p class="error">{{ errors.password | first }}</p>
     <div class="form-actions">
       <router-link to="">
         Esqueci minha senha
       </router-link>
-      <button @click.prevent="postLogin">Entrar</button>
+      <button>Entrar</button>
     </div>
   </form>
 </template>
 
 <script>
 import Cookie from 'js-cookie'
+import validate from 'validate.js'
+
+const constraints = {
+  email: {
+    presence: { message: '^E-mail não pode ser vazio' },
+    email: true,
+  },
+  password: {
+    presence: { message: '^Password não pode ser vazio' },
+    length: {
+      minimum: 6,
+      message: 'must be at least 6 characters',
+    },
+  },
+}
 
 export default {
   middleware: 'notAuthenticated',
+  data() {
+    return {
+      errors: {},
+    }
+  },
   methods: {
+    checkForm() {
+      console.log(this.email, this.password)
+
+      this.errors = validate(
+        {
+          email: this.email,
+          password: this.password,
+        },
+        constraints,
+      )
+    },
     postLogin() {
       setTimeout(() => {
         // we simulate the async request with timeout.
--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ['~/plugins/axios'],
+  plugins: ['~/plugins/axios', '~/plugins/filters'],
   /*
    ** Nuxt.js dev-modules
    */
