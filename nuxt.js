7ea58cccd24ac76f0ce3b20590c6337cd98c8231
Carlos José <carlosxjm@gmail.com>
2019-12-04 20:49:53 -0300

    Removing eslint module from lambda buildDependency

--- nuxt.js
+++ nuxt.js
@@ -1,8 +1,12 @@
-const { Nuxt } = require('nuxt-start')
+const { Nuxt } = require('@nuxt/core')
 
 const config = require('./nuxt.config.js')
 
-const nuxt = new Nuxt({ ...config, dev: false })
+const nuxt = new Nuxt({
+  ...config,
+  buildModules: [],
+  dev: false,
+})
 
 module.exports = (req, res) =>
   nuxt.ready().then(() => nuxt.server.app(req, res))
