314de71caa1ab80cfdf553cbf369ca385c885caf
Carlos José <carlosxjm@gmail.com>
2019-12-17 19:24:04 -0300

    Splitting login and register into different pages

---
+++ components/AuthPage.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="auth-page">
+    <div class="auth-page-form-wrapper content">
+      <slot />
+    </div>
+    <div class="auth-page-info-wrap">
+      <div class="auth-page-info">
+        <h1 class="title">{{ title }}</h1>
+        <p>
+          {{ description }}
+        </p>
+        <router-link
+          :to="ctaConfig.path"
+          tag="button"
+          class="button button-large button-clear button-black"
+        >
+          {{ ctaConfig.message }}
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+    ctaConfig: {
+      type: Object,
+      default: () => ({
+        message: '',
+      }),
+    },
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+@import '~/assets/sass/variables'
+
+.auth-page
+  display: flex
+  flex-direction: column
+  height: calc(100vh - #{$header-height})
+  @media (min-width: $content-width)
+    flex-direction: row
+  .auth-page-info-wrap
+    display: flex
+    align-items: center
+    justify-content: center
+    flex-direction: column
+    min-width: 50%
+    background-color: $color-primary
+    @media (max-width: 900px)
+      min-width: 100%
+      height: 60px
+    .login-form
+      height: 100%
+    .auth-page-info
+      display: flex
+      align-items: center
+      justify-content: center
+      flex-direction: column
+      max-width: 500px
+      margin: 0 auto
+      color: #FFF
+      @media (max-width: 900px)
+        .title,
+        p
+          display: none
+      h1
+        font-size: 4rem
+        font-weight: 600
+        margin-bottom: 30px
+      p
+        text-align: center
+        font-size: 2rem
+        font-weight: 400
+        margin-bottom: 30px
+      button
+        margin: 0
+        color: white
+
+  .auth-page-form-wrapper
+    min-width: 50%
+    height: 100%
+    display: flex
+    align-items: center
+    justify-content: center
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,103 +1,26 @@
 <template>
-  <div class="login-page">
-    <div class="login-page-form-wrapper content">
-      <RegisterForm v-if="isNewUser" />
-      <LoginForm v-else />
-    </div>
-    <div class="login-page-info-wrap">
-      <div class="login-page-info">
-        <h1 class="title">Cadastre vagas!</h1>
-        <p v-if="isNewUser">
-          Cadastre sua empresa gratuitamente e comece a divulgar vagas. Caso já
-          tenha uma conta clique na opção abaixo:
-        </p>
-        <p v-else>
-          Para cadastrar vagas você precisa fazer login, caso não tenha uma
-          conta clique na opção abaixo:
-        </p>
-        <button
-          @click="toogleIsNewUser"
-          class="button button-large button-clear button-black"
-        >
-          {{ isNewUser ? 'Já tenho uma conta' : 'Quero me cadastrar' }}
-        </button>
-      </div>
-    </div>
-  </div>
+  <AuthPage
+    :ctaConfig="{
+      message: 'Quero me cadastrar',
+      path: 'register',
+    }"
+    description="
+      Para cadastrar vagas você precisa fazer login, caso não tenha uma conta clique na opção abaixo:
+    "
+    title="Cadastre vagas!"
+  >
+    <LoginForm />
+  </AuthPage>
 </template>
 
 <script>
+import AuthPage from '~/components/AuthPage'
 import LoginForm from '~/components/LoginForm'
-import RegisterForm from '~/components/RegisterForm'
 
 export default {
   components: {
+    AuthPage,
     LoginForm,
-    RegisterForm,
-  },
-  data() {
-    return {
-      isNewUser: false,
-    }
-  },
-  methods: {
-    toogleIsNewUser() {
-      this.isNewUser = !this.isNewUser
-    },
   },
 }
 </script>
-
-<style lang="sass" scoped>
-@import '~/assets/sass/variables'
-
-.login-page
-  display: flex
-  flex-direction: column
-  height: calc(100vh - #{$header-height})
-  @media (min-width: $content-width)
-    flex-direction: row
-  .login-page-info-wrap
-    display: flex
-    align-items: center
-    justify-content: center
-    flex-direction: column
-    min-width: 50%
-    background-color: $color-primary
-    @media (max-width: 900px)
-      min-width: 100%
-      height: 60px
-    .login-form
-      height: 100%
-    .login-page-info
-      display: flex
-      align-items: center
-      justify-content: center
-      flex-direction: column
-      max-width: 500px
-      margin: 0 auto
-      color: #FFF
-      @media (max-width: 900px)
-        .title,
-        p
-          display: none
-      h1
-        font-size: 4rem
-        font-weight: 600
-        margin-bottom: 30px
-      p
-        text-align: center
-        font-size: 2rem
-        font-weight: 400
-        margin-bottom: 30px
-      button
-        margin: 0
-        color: white
-
-  .login-page-form-wrapper
-    min-width: 50%
-    height: 100%
-    display: flex
-    align-items: center
-    justify-content: center
-</style>
