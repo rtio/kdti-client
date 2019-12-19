b5103076d5339185fb4b26eb1cbf5fda18643750
Carlos José <carlosxjm@gmail.com>
2019-12-19 12:46:50 -0300

    Fixing request auth

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,6 +2,17 @@ import createRepository from '~/api/repository'
 export default (ctx, inject) => {
   // And in the Vue instances (this.$repository in your components)
   const repositoryWithAxios = createRepository(ctx.$axios)
+
+  ctx.$axios.interceptors.request.use(
+    (config) => {
+      if (ctx.store.state.auth) {
+        config.headers.common.Authorization = `Bearer ${ctx.store.state.auth}`
+      }
+      return config
+    },
+    (error) => Promise.reject(error),
+  )
+
   inject('jobRepository', repositoryWithAxios('/job-offers'))
   inject('accountRepository', {
     create(payload) {
--- store/index.js
+++ store/index.js
@@ -1,24 +1,37 @@
 import Cookie from 'js-cookie'
 
+const parseCookies = (cookie, key) => {
+  const output = {}
+  cookie.split(/\s*;\s*/).forEach(function(pair) {
+    pair = pair.split(/\s*=\s*/)
+    output[pair[0]] = pair.splice(1).join('=')
+  })
+  return output[key]
+}
+
 export const state = () => {
   return {
-    auth: null,
+    auth: Cookie.get('auth'),
   }
 }
+
 export const mutations = {
   setAuth(state, auth) {
     Cookie.set('auth', auth)
     state.auth = auth
   },
 }
+
 export const actions = {
   nuxtServerInit({ commit }, { req }) {
     let auth = null
+
     if (req.headers.cookie) {
       try {
-        auth = JSON.parse(Cookie.get('auth'))
+        auth = parseCookies(req.headers.cookie, 'auth')
       } catch (err) {
         // No valid cookie found
+        console.err(err)
       }
     }
     commit('setAuth', auth)
