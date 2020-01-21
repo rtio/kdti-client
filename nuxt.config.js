7f640102b1bed6c646bd0d7a561f660d789e7570
Allan <allan.hal@gmail.com>
2020-01-20 23:28:23 -0300

    Faking calls with json-server

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,7 @@
+require('dotenv').config()
+
+const { BASE_URL = 'https://staging.kdti.dev/api/' } = process.env
+
 // eslint-disable-next-line nuxt/no-cjs-in-config
 module.exports = {
   mode: 'universal',
@@ -57,7 +61,7 @@ module.exports = {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseURL: 'https://staging.kdti.dev/api/',
+    baseURL: BASE_URL,
   },
   /*
    ** Build configuration
