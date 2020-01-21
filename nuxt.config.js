8dfa69aa36ab3fba82378226d8ab83473c777b36
Rafael Meneses <meneses.tio@gmail.com>
2020-01-21 19:17:30 -0300

    prepare staging env

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,7 @@ module.exports = {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseURL: 'https://staging.kdti.dev/api/',
+    baseURL: process.env.API_URL || 'http://localhost:8000/api/',
   },
   /*
    ** Build configuration
